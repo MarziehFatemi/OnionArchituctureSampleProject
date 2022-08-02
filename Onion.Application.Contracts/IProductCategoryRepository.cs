1729db53da1dcdff20931720678c4aee781c1552
marziehfatemi@gmail.com <marziehfatemi@gmail.com>
2022-08-02 12:38:53 +0430

    Application Layer contract class library created
    some command classes for create, edit and view model is created
    
    an interface repository for create and edit based on the command classes are created
    it has also contains a function with search name that give view model based on name

---
+++ Onion.Application.Contracts/IProductCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onion.Application.Contracts
+{
+    public interface IProductCategoryRepository
+    {
+       
+        void Create(CreateProductCategoryCommand Command);
+        void Edit (EditProductCategoryCommand Command);
+        List<ProdcutCategoryViewModel> Search(string name); 
+
+    }
+}
