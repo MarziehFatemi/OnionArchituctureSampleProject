b4dee5e5b73a797246d8e82199f232f872c2fbe7
marziehfatemi@gmail.com <marziehfatemi@gmail.com>
2022-08-02 12:22:38 +0430

    Domain layer created product and product category model with private set and intenal bussiness logic are created inteface repostitory for product and product category are also created with abstract functions of get and create

---
+++ Onion.Domain/Product Category agg/IProductCategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Onion.Domain.Product_Category_agg
+{
+    public interface IProductCategoryRepository
+    {
+         ProductCategory Get(int id);
+        void Create(ProductCategory productCategory);
+
+    }
+}
