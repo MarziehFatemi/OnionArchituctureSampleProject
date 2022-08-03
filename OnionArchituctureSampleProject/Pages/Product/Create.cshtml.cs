9f40c34cb22ed05bd6c79aac29fe02ff0eea1870
marziehfatemi@gmail.com <marziehfatemi@gmail.com>
2022-08-03 17:36:33 +0430

    Create OnPost Added and it is working

--- OnionArchituctureSampleProject/Pages/Product/Create.cshtml.cs
+++ OnionArchituctureSampleProject/Pages/Product/Create.cshtml.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Onion.Application.Contracts;
 using Onion.Application.Contracts.ProductApplication_Agg;
 
@@ -7,19 +8,47 @@ namespace OnionArchituctureSampleProject.Pages.Product
 {
     public class CreateModel : PageModel
     {
+        public SelectList ProductCategories; 
         public CreateProductCommand Command { get; set; }
 
 
 
         private readonly IProductAppication _IProductApplication;
+        private readonly IProductCategoryApplication _IProductCategoryApplication;
 
-        public CreateModel(IProductAppication productAppication)
+
+        public CreateModel(IProductAppication productAppication, IProductCategoryApplication iProductCategoryApplication)
         {
             _IProductApplication = productAppication;
-
+            _IProductCategoryApplication = iProductCategoryApplication;
         }
         public void OnGet()
         {
+            ProductCategories = new SelectList
+                (_IProductCategoryApplication.GetAll(), "Id", "Name"); 
+        }
+
+        public IActionResult OnPost(CreateProductCommand Command)
+        {
+            if ( ModelState.IsValid)
+            {
+                string Error = "";
+                if (_IProductApplication.Create(Command, out Error))
+                { return RedirectToPage("/Product/Index"); }
+                else
+                {
+                    ViewData["Error"] = Error;
+                    return Page();
+
+                }
+
+            }
+
+            else 
+            {
+                ViewData["Error"] = "مشکلی پیش امده مجدد تلاش کنید";
+                return Page(); 
+            }
         }
     }
 }
